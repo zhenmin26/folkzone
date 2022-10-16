e21a880ec05aeff39947929708f7e733a149ccc6
zhenmin26 <zhenmin@zhenmins-mbp.dyndns.rice.edu>
2022-10-16 09:56:17 -0500

    downgrade react to v17 and apply redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,17 @@
-import { createStore } from 'redux'
+import { createStore } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-import rootReducer from './rootReducer'
+import rootReducer from "./rootReducer";
 
-export default createStore(rootReducer)
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const myPersistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(myPersistedReducer)
+
+export const persistor = persistStore(store)
+export default store
